cd55266d764e999540924c901d0f517afdf8e35b
stephen <stephen1@jiangstars.com>
2025-12-07 16:30:19 +0800

    【功能】增加wujie框架

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    cors: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  },
+  base: '/',
+  build: {
+    target: 'es2015',
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
